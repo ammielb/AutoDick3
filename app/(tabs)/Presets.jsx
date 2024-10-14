ec4f28e6c23753de215335eec5e75a595855b69f
Ivo Bruinsma <1082102@hr.nl>
2024-10-14 14:05:09 +0200

    Invoegen navbar

---
+++ app/(tabs)/Presets.jsx
@@ -0,0 +1,12 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const Presets = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Presets Screen</Text>
+  </View>
+  );
+}
+
+export default Presets
