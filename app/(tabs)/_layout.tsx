4dfa6df37e009c2970f093d8125bb70ff1733abb
Ivo Bruinsma <1082102@hr.nl>
2024-10-14 14:05:09 +0200

    Invoegen navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,13 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Stack } from 'expo-router'; // Stack from Expo Router
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function Layout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <Stack>
+      {/* Automatically maps to app/index.jsx */}
+      <Stack.Screen name="index" options={{ title: 'index' }} />
+
+      {/* Automatically maps to app/other.jsx */}
+      <Stack.Screen name="Presets" options={{ title: 'Presets' }} />
+    </Stack>
   );
 }
